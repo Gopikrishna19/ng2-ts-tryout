7ea57b8c3c78a65b514e54e2f0536cbced1ed61e
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-02 01:24:25 -0500

    get hero from route

--- app/components/hero-detail.component.ts
+++ app/components/hero-detail.component.ts
@@ -1,5 +1,8 @@
+import {HeroService} from '../services/hero.service';
 import {IHero} from '../types/IHero';
-import {Component, Input} from '@angular/core';
+import {Location} from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'my-hero-detail',
@@ -14,7 +17,25 @@ import {Component, Input} from '@angular/core';
 </div>
 `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input()
   private hero: IHero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  private ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  private goBack(): void {
+    this.location.back();
+  }
 }
