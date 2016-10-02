60dc1e0caa23f66848863fc236867e7619f910c8
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-02 01:25:53 -0500

    extract html template

--- app/components/hero-detail.component.ts
+++ app/components/hero-detail.component.ts
@@ -5,17 +5,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-<div *ngIf="hero">
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name"/>
-  </div>
-</div>
-`
+  templateUrl: 'hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
   @Input()
