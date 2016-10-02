49bff4ca1a99e76a279a37c52e32824c78252117
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-01 23:34:42 -0500

    include form modules for using form related directive like ng-model

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import {AppComponent} from './app.component';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
-    bootstrap: [AppComponent],
-    declarations: [AppComponent],
-    imports: [BrowserModule]
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ]
 })
 export class AppModule {
 }
