757abf57b176789724da52c0d4cd198fde46655b
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-02 09:02:33 -0500

    get data from api instead of collection

--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -1,17 +1,35 @@
-import {HEROES} from '../data/heroes';
 import {IHero} from '../types/IHero';
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
+  private url = '/app/heroes';
+
+  constructor(private http: Http) {}
+
   public getHeroes(): Promise<IHero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.url)
+      .toPromise()
+      .then(
+        response => response.json().data as IHero[],
+        this.handleError
+      );
   }
 
   public  getHero(id: number): Promise<IHero> {
-    return this.getHeroes()
-      .then(heroes =>
-        heroes.find(hero => hero.id === id)
+    const url = `${this.url}/${id}`;
+
+    return this.http.get(url)
+      .toPromise()
+      .then(
+        response => response.json().data as IHero[],
+        this.handleError
       );
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
   }
 }
